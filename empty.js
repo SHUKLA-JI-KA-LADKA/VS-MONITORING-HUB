3f0f11c80230592073f3be047f74f1de9472bb29
VAIBHAV <65447291+SHUKLA-JI-KA-LADKA@users.noreply.github.com>
2021-07-20 20:45:18 +0530

    Update empty.js

--- empty.js
+++ empty.js
@@ -1,1 +1,11 @@
 
+  width:500,
+    height:500,
+    minWidth:400,
+    maxWidth:1920,
+    minHeight:300,
+    maxHeight:1080,
+    backgroundColor: "#3c3c44",
+    webPreferences:{
+      nodeIntegration:true,
+      contextIsolation:false
