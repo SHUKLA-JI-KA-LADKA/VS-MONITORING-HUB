d80ec27c9191f67f8028baf48bcad22cfd678560
VAIBHAV <65447291+SHUKLA-JI-KA-LADKA@users.noreply.github.com>
2021-07-20 20:47:10 +0530

    Update empty.js

--- empty.js
+++ empty.js
@@ -1,11 +1,1 @@
-
-  width:500,
-    height:500,
-    minWidth:400,
-    maxWidth:1920,
-    minHeight:300,
-    maxHeight:1080,
-    backgroundColor: "#3c3c44",
-    webPreferences:{
-      nodeIntegration:true,
-      contextIsolation:false
+///did'nt u read the title it's empty mate nothing to see here.    u got bamboozeld
